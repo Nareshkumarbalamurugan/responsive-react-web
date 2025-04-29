76b52b65494b2498c97ab0e3070e192e79ee4b4a
Nareshkumar Balamurugan <nareshkumarbalamurugan@gmail.com>
2025-04-29 19:08:24 +0530

    created

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/responsive-react-web/", // <-- ADD THIS LINE
+  base: "/responsive-react-web/",
   server: {
     host: "::",
     port: 8080,
